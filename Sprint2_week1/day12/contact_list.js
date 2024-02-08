0df8da3675e626abbe2d96a4ea4abf43740f1af3
fadwa3 <benlhaddadf@gmail.com>
2024-02-08 14:17:16 +0100

    update 1  day 12 tasks

--- Sprint2_week1/day12/contact_list.js
+++ Sprint2_week1/day12/contact_list.js
@@ -1,12 +1,49 @@
-const { exit } = require('process');
 const reader = require('readline');
-// Create the readline interface
+//! methode 1 
+//! start
+//* Create the readline interface
 const rl = reader.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+//* the enter contact information function
+
+function EnterInformations() {
+    rl.question('What is your name? ', (name) => {
+        rl.question('What is your phone number? ', (number) => {
+            collection.push({ name, number });
+            Menu();
+        });
+
+    });
+}
+//* the display contacts function
+
+function displaycontacts() {
+    if (collection.length != 0) { 
+        collection.forEach((element) => console.log(`contact name: ${element.name}, Phone number: ${element.number}`));
+    } else {
+        console.log('No contacts available.');
+    }
+    Menu();
+}
+//* the search contact information function
+
+function searchcontact() {
+    rl.question('What is the name of the contact searched? ', (nm) => {
+        const contact = collection.find((c) => c.name === nm);
+        if (contact) {
+            console.log(`The contact searched with the name: ${contact.name} and the phone number: ${contact.number}`);
+        } else {
+            console.log("The contact searched doesn't exist");
+        }
+        Menu();
+
+    });
+}
 
 let collection = [];
+//* the contact menu function
 function Menu() {
     console.log('Hello and welcome to the contact list menu: \n1 --> Enter a new contact \n2 --> Display the list of contacts \n3 --> Search for a contact \n4 --> Exit the application');
     rl.question('Write the number of choice you want: ', (choice_nbr) => {
@@ -22,50 +59,14 @@ function Menu() {
                 searchcontact();
                 break;
             case 4:
-                exit();
+                rl.close();
+                break;
             default:
                 console.log('The choice number you entered is invalid');
+            Menu();
         }
-        Menu();
     });
 }
-
-function EnterInformations() {
-    rl.question('What is your name? ', (name) => {
-        rl.question('What is your phone number? ', (number) => {
-            collection.push({ name, number });
-            rl.close();
-        });
-    });
-    Menu();
-
-}
-
-function displaycontacts() {
-    if (collection.length == 0) {
-        console.log('No contacts available.');
-    } else {
-        collection.forEach((element) => console.log(`contact name: ${element.name}, Phone number: ${element.number}`));
-    }
-    Menu();
-
-}
-
-function searchcontact() {
-    rl.question('What is the name of the contact searched? ', (nm) => {
-        const contact = collection.find((c) => c.name === nm);
-        if (contact) {
-            console.log(`The contact searched with the name: ${contact.name} and the phone number: ${contact.number}`);
-        } else {
-            console.log("The contact searched doesn't exist");
-        }
-        rl.close();
-    });
-    Menu();
-
-}
+//*calling the menu function
 Menu();
-
-rl.on('close', () => {
-    exit();
-});
+//!end 
