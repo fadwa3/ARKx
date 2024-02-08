51fd73a60bf64d1283e8ff9a3e9786a7b51a7b35
fadwa3 <benlhaddadf@gmail.com>
2024-02-08 22:35:53 +0100

    add day 14 tasks

--- Sprint2_week1/day13/weather_display.js
+++ Sprint2_week1/day13/weather_display.js
@@ -10,22 +10,20 @@ const cities = [
     { name: 'Rio de Janeiro', lat: -22.9068, lng: -43.1729 },
     { name: 'Dubai', lat: 25.2048, lng: 55.2708 },
     { name: 'Rabat', lat: 34.0209, lng: -6.8416 }
-  ];
- 
-async function display_weather(city){
-try{   
-const apiKey = 'bde7e9114a6d3c2e45b15cade4778466';
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lng}&appid=${apiKey}&units=metric`;
-
-const response = await fetch(apiUrl);
-const data = await response.json();
-const temp=data.main.temp;
-const city=data.name;
-return ({city,temp})
-
-}catch(error){
-    throw new Error('failed to fetch the data from weather api');
-}
+];
+//* the main function
+async function display_weather(c) {
+    try {
+        const apiKey = 'bde7e9114a6d3c2e45b15cade4778466';
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${c.lat}&lon=${c.lng}&appid=${apiKey}&units=metric`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const temp = data.main.temp;
+        const city = data.name;
+        return { city, temp }
+    } catch (error) {
+        throw new Error('failed to fetch the data from weather api');
+    }
 }
 
 //* Function to select a city randomly
@@ -33,13 +31,13 @@ function selectRandomCity(cities) {
     const randomIndex = Math.floor(Math.random() * cities.length);
     return cities[randomIndex];
 }
-
+//* calling the function to display the result
 display_weather(selectRandomCity(cities))
-.then((data)=>{
-    console.log('--> Here is the weather of a random city : ');
-    console.log(`${data.city}  : ${data.temp}`);
-})
-.catch((error)=>{
-  console.log('--> ooops here is a bug :( ')
-  console.log(error);
-})
+    .then(({ city, temp }) => {
+        console.log('--> Here is the weather of a random city : ');
+        console.log(`${city}  : ${temp}`);
+    })
+    .catch((error) => {
+        console.log('--> ooops here is a bug :( ')
+        console.log(error);
+    })
---
+++ Sprint2_week1/day14/weather_display_day14.js
@@ -0,0 +1,77 @@
+const fs = require('fs');
+
+//* List of cities to be used
+const cities = [
+    { name: 'New York', lat: 40.7128, lng: -74.0060 },
+    { name: 'London', lat: 51.5074, lng: -0.1278 },
+    { name: 'Paris', lat: 48.8566, lng: 2.3522 },
+    { name: 'Tokyo', lat: 35.6895, lng: 139.6917 },
+    { name: 'Sydney', lat: -33.8651, lng: 151.2099 },
+    { name: 'Rome', lat: 41.9028, lng: 12.4964 },
+    { name: 'Cairo', lat: 30.0444, lng: 31.2357 },
+    { name: 'Rio de Janeiro', lat: -22.9068, lng: -43.1729 },
+    { name: 'Dubai', lat: 25.2048, lng: 55.2708 },
+    { name: 'Rabat', lat: 34.0209, lng: -6.8416 }
+];
+
+//* retrieve the data from the api
+async function display_weather(city_name) {
+    try {
+        const apiKey = 'bde7e9114a6d3c2e45b15cade4778466';
+        const city = cities.find(c => c.name == city_name);
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lng}&appid=${apiKey}&units=metric`;
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const temp = data.main.temp;
+
+        return temp;
+    } catch (error) {
+        throw new Error('bug : failed to fetch the data from weather api' ,error.message);
+    }
+}
+
+//* delete existing files with the city name
+function deleteCityFile(cityName) {
+    const filename = `${cityName}.txt`;
+    try {
+        fs.unlinkSync(filename);
+    } catch (error) {
+    }
+}
+
+//* write result in a new file
+function write_to_File(cityName, temperature) {
+    const filename = `${cityName}.txt`;
+    fs.writeFileSync(filename, `Temperature in ${cityName}: ${temperature} C`);
+}
+//* function to select a random city from an array
+function selectRandomCity(cities) {
+    const randomIndex = Math.floor(Math.random() * cities.length);
+    return cities[randomIndex];
+}
+//* read array of city names from "input.txt" and do the magic 
+fs.readFile('input.txt', 'utf8', (err, data) => {
+    if (err) {
+        throw new Error('bug : failure in reading the input.txt:');
+       
+    }
+    try {
+        const city_Names = data; 
+        const random_city = selectRandomCity(JSON.parse(city_Names)); 
+
+      //* retrieve the weather data 
+        display_weather(random_city)
+            .then(temp => {
+                deleteCityFile(random_city);
+                write_to_File(random_city, temp);
+                console.log('the new file was created succesfuly :D')
+            })
+            .catch(error => {
+                console.error('bug : error in display weather function', error.message);
+            });
+    } catch (error) {
+        console.error('bug : failure parsing the file data  ', error.message);
+    }
+});
+
+
