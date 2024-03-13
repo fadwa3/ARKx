14e37ebc8c152cab91e48134756d714c1ff057ad
fadwa3 <benlhaddadf@gmail.com>
2024-03-13 10:25:49 +0000

    add html+css challenge version 1

--- Sprint3_week1/Day25/day25.js
+++ Sprint3_week1/Day25/day25.js
@@ -16,10 +16,10 @@ var flash = require('connect-flash');
 const app = express();
 //! Middlewares
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 app.use(flash());
 
-app.use(express.urlencoded({ extended: false }));
+
 app.use(
     session({
         isconnected: false,
